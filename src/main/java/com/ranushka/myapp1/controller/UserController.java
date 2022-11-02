c9b8265b675f3c54444c55efca3f6b7fd8f37c11
ranushka-lakmal <ranulakmal@gmail.com>
2022-11-02 22:24:21 +0530

    add getuser

---
+++ src/main/java/com/ranushka/myapp1/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.ranushka.myapp1.controller;
+
+import ch.qos.logback.core.boolex.EvaluationException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+
+public class UserController {
+
+	@GetMapping("/getUser")
+	public String getUser(){
+			return "getUser Name";
+	}
+
+}
