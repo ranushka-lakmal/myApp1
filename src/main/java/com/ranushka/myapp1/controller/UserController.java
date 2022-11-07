816da8bd5a04d64f275c869c144bc05df03693c9
ranushka-lakmal <ranulakmal@gmail.com>
2022-11-07 22:08:39 +0530

    initial code

--- src/main/java/com/ranushka/myapp1/controller/UserController.java
+++ src/main/java/com/ranushka/myapp1/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.ranushka.myapp1.controller;
 
 import ch.qos.logback.core.boolex.EvaluationException;
+import com.ranushka.myapp1.dto.UserDTO;
+import com.ranushka.myapp1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,14 +12,18 @@ import org.springframework.web.bind.annotation.*;
 
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/getUser")
 	public String getUser(){
 			return "Ranushka";
 	}
 
 	@PostMapping("/saveUser")
-	public String saveUser(){
-		return "User Save!";
+	public UserDTO saveUser(@RequestBody UserDTO userDTO){
+		return userService.saveUser(userDTO);
+
 	}
 
 	@PutMapping("/updateUser")
---
+++ src/main/java/com/ranushka/myapp1/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.ranushka.myapp1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDTO {
+	@Id
+	private int id;
+	private String name;
+	private String address;
+}
